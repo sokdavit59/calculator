d6d72b944c500285f21c7f1065d463b4d4ec737e
sokdavit59 <128958016+sokdavit59@users.noreply.github.com>
2023-09-22 12:32:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let displayValue = "";
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById("display").value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = "";
+    document.getElementById("display").value = displayValue;
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+        document.getElementById("display").value = displayValue;
+    } catch (error) {
+        displayValue = "Error";
+        document.getElementById("display").value = displayValue;
+    }
+}
